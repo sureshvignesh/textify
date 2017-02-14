7dea9188f707bddecd5f22220de15ff960dc3040
vinodprak <vinod@vakilsearch.com>
2017-02-14 13:02:49 +0530

    Section Id Classfied

--- app/assets/javascripts/ckeditor/plugins/section/plugin.js
+++ app/assets/javascripts/ckeditor/plugins/section/plugin.js
@@ -1,24 +1,40 @@
-(function(){
-var a=
-{
-    exec:function(editor){
-        var format = {
-        element : "div"
-        };
-    var style = new CKEDITOR.style(format);
-    style.apply(editor.document);
-    }
-},
+(function() {
+   var c = {
+           exec: function(editor) {
+               var a = CKEDITOR.instances.editor.getSelectedHtml();
+               var c_length = a.$.children.length
+               var merge_text = ""
+               var first_line = ""
+               for (i = 0; i < c_length; i++) {
+                   merge_text += "<p>"
+                   merge_text += a.$.children[i].textContent
+                   merge_text += "</p>"
+                   merge_text += "\n"
+                   merge_text += "\n"
+               }
+               first_line = a.$.children[0].textContent
+               var document_text = CKEDITOR.instances.editor.getData();
+               var merge = merge_text.replace(/<p>/g, "");
+               merge = merge_text.replace(/<\/?p[^>]*>/g, "");
+               merge = "<div id="+first_line.split(' ')[0]+" >" + merge + "</div>"
 
-b="section";
-CKEDITOR.plugins.add(b,{
-    init:function(editor){
-    editor.addCommand(b,a);
-    editor.ui.addButton(b,{
-    label:"Section",
-    icon: this.path + "section.png",
-    command:b
-    });
-    }
-});
+               var text = document_text.replace(merge_text, merge);
+               CKEDITOR.instances['editor'].setData(text);
+               console.log(merge_text);
+               console.log(merge);
+           }
+       },
+
+       b = "section";
+   CKEDITOR.plugins.add(b, {
+       init: function(editor) {
+           editor.addCommand(b, c);
+           editor.ui.addButton(b, {
+               label: "Section",
+               icon: this.path + "section.png",
+               command: b
+           });
+
+       }
+   });
 })();
